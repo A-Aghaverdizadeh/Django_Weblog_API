11bf34ccad368edd8ec25488e1caee9c3c6009f9
AmirAghaverdi <amiraghaverdigit@gmail.com>
2026-06-26 19:44:27 +0330

    made improvements to configuration of the project

--- core/settings.py
+++ core/settings.py
@@ -74,25 +74,25 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.{}'.format(
-             config('DATABASE_ENGINE', 'sqlite3')
-         ),
-         'NAME': config('DATABASE_NAME', 'polls'),
-         'USER': config('DATABASE_USERNAME', 'core'),
-         'PASSWORD': config('DATABASE_PASSWORD', 'Aa13311331@'),
-         'HOST': config('DATABASE_HOST', '127.0.0.1'),
-         'PORT': config('DATABASE_PORT', 5432),
-     }
- }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.{}'.format(
+#              config('DATABASE_ENGINE', 'sqlite3')
+#          ),
+#          'NAME': config('DATABASE_NAME', 'polls'),
+#          'USER': config('DATABASE_USERNAME', 'core'),
+#          'PASSWORD': config('DATABASE_PASSWORD', 'Aa13311331@'),
+#          'HOST': config('DATABASE_HOST', '127.0.0.1'),
+#          'PORT': config('DATABASE_PORT', 5432),
+#      }
+#  }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 # Stage 1: Base build stage
-FROM python:3.13-slim
+FROM docker.arvancloud.ir/python:3.13-slim
  
 # Create the app directory
 RUN mkdir /app
@@ -12,13 +12,13 @@ ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1 
  
 # Upgrade pip and install dependencies
-RUN pip install --upgrade pip 
+RUN pip install --upgrade pip -i https://mirror-pypi.runflare.com/simple
  
 # Copy the requirements file first (better caching)
 COPY requirements.txt /app/
  
 # Install Python dependencies
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt -i https://mirror-pypi.runflare.com/simple
 
 WORKDIR /app
 
